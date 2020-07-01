5dbefe72ab5a5ea973e41ce38f7589450244a2aa
george <gnicolaou@axiomc.com>
2020-07-01 12:03:09 +0300

    jkhsdrjghfd

--- SPMLib/TestPodWithSPM/Package.swift
+++ SPMLib/TestPodWithSPM/Package.swift
@@ -7,7 +7,9 @@ let package = Package(
     name: "TestPodWithSPM",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(name: "TestPodWithSPM", targets: ["TestPodWithSPM"]),
+        .library(
+            name: "TestPodWithSPM",
+            targets: ["TestPodWithSPM"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -16,6 +18,8 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "TestPodWithSPM", dependencies: []),
+        .target(
+            name: "TestPodWithSPM",
+            dependencies: []),
     ]
 )
